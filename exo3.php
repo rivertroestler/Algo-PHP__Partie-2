d1662ef243ce5f75403a38e263b1f2a0c10ac7be
rivertroestler <167063264+rivertroestler@users.noreply.github.com>
2024-04-17 15:37:00 +0200

    exo 3
    html hypertext link

---
+++ exo3.php
@@ -0,0 +1,16 @@
+<h1>Exercice 2</h1>
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank). <br>
+</p>
+
+<h2>Résultat</h2>
+
+<a href="https://elan-formation.fr/"target="_blank">Elan Formation</a>
+
+
+<?php
+
+$Elan = array(
+    "title" => "Elan Formation",
+    "href" => "https://elan-formation.fr/"
+);
