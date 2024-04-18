15db49e6ecdcc876aa1da7c0daa878b7021874d3
rivertroestler <167063264+rivertroestler@users.noreply.github.com>
2024-04-18 10:59:54 +0200

    exo3.php
    corrigé

--- exo3.php
+++ exo3.php
@@ -1,16 +1,13 @@
-<h1>Exercice 2</h1>
+<h1>Exercice 3</h1>
 <p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
 dans un nouvel onglet (_blank). <br>
 </p>
 
 <h2>Résultat</h2>
 
-<a href="https://elan-formation.fr/"target="_blank">Elan Formation</a>
-
+$url = "https://elan-formation.fr/";
 
 <?php
+$url = "https://elan-formation.fr/";
 
-$Elan = array(
-    "title" => "Elan Formation",
-    "href" => "https://elan-formation.fr/"
-);
+echo "<a href=".$url.">Elan Formation</a>";
